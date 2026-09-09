3288a72d8e89fb6d8ab9cda18c3f678d295b0c33
Alex Hyafil <alexandre.hyafil@gmail.com>
2026-09-09 14:40:19 +0200

    Update team-data.js

--- js/team-data.js
+++ js/team-data.js
@@ -69,7 +69,7 @@ photo: "assets/people/babyalex.jpg",
     bio: "Anamaria holds a BSc in Psychology from the University of York and an MSc in Brain and Cognition from Universitat Pompeu Fabra. For her Master’s thesis she investigated the role of midfrontal theta-band oscillations as a marker of evidence accumulation in perceptual decision-making. She is now joining the lab as a research assistant, where she will continue to build upon this project.",
 	links: {
       email: "",
-      website: "",
+      website: "www.linkedin.com/in/anamaria-isaia",
       github: "",
       bluesky: "",
       scholar: ""
